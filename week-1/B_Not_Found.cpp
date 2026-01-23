9fcdb05279aa0c9cb2573ebb27f32ccc656d70ce
ThakurSaad <thakursaad613@gmail.com>
2026-01-23 12:33:14 +0600

    new code

--- week-1/B_Not_Found.cpp
+++ week-1/B_Not_Found.cpp
@@ -3,6 +3,14 @@ using namespace std;
 
 int main()
 {
-    
+    bool condition = false;
+    if (condition)
+    {
+        cout << "Path found!" << endl;
+    }
+    else
+    {
+        cout << "This path is not found." << endl;
+    }
     return 0;
 }
