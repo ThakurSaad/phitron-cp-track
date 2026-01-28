930ab54ce1f5b58e9f388d3635fffcb44049b87d
ThakurSaad <thakursaad613@gmail.com>
2026-01-28 21:18:20 +0600

    adding 123 to 4

---
+++ Cashback.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int x;
+    cin >> x;
+
+    if (x >= 200)
+        cout << x - 50;
+    else
+        cout << x;
+
+    return 0;
+}
